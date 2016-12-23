598814bde43c208abc3e9ebde26e75d828355ed3
Ruben Amortegui <ruben.amortegui@gmail.com>
2016-12-22 22:52:19 -0700

    Update item quantity if exists.

--- lib/ec_cart.ex
+++ lib/ec_cart.ex
@@ -1,8 +1,8 @@
 
 defmodule EcCartItem do
-  defstruct ec_sku: nil, ec_price: 0, ec_quantity: 1, attr: %{}
-  def new( ec_sku, ec_price, ec_quantity, attr) do
-    %EcCartItem{ ec_sku: ec_sku, ec_price: ec_price, ec_quantity: ec_quantity, attr: attr }
+  defstruct ec_sku: nil, ec_price: 0, ec_qty: 1, attr: %{}
+  def new( ec_sku, ec_price, ec_qty, attr) do
+    %EcCartItem{ ec_sku: ec_sku, ec_price: ec_price, ec_qty: ec_qty, attr: attr }
   end
 end
 
@@ -10,9 +10,17 @@ defmodule EcCart do
   defstruct items: []
   def new, do: %EcCart{}
   def addItem( %EcCart{ items: items }, %EcCartItem{} = ec_cart_item ) do
-    %EcCart{
-      items: items++[ec_cart_item]
-    }
+    index = Enum.find_index( items, fn(item) -> item.ec_sku == ec_cart_item.ec_sku end )
+    case index do
+      nil ->
+        %EcCart{
+          items: items++[ec_cart_item]
+        }
+      x ->
+        item = Enum.at( items, x )
+        item = %EcCartItem{ item | ec_qty: ( item.ec_qty + ec_cart_item.ec_qty ) }
+        %EcCart{ items: List.update_at(items, x, fn(old_item) -> %EcCartItem{ old_item | ec_qty: item.ec_qty } end )}
+    end
   end
 end
 
