c8ec464ba50aad51323ba04e6121efa6c3742f10
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-01-04 22:09:04 -0700

    Add documentation for Ec.Cart.Adjustment module.

--- lib/ec/cart/adjustment.ex
+++ lib/ec/cart/adjustment.ex
@@ -1,5 +1,19 @@
 defmodule Ec.Cart.Adjustment do
+  @moduledoc """
+  Definition of the structur of an adjustment
+  """
   defstruct name: nil, description: nil, function: nil
+
+  @doc """
+    Creates a structure based on:
+    
+    `name`  Name of the adjustment
+
+    `description` Description of the adjustment
+
+    `function` Anonymous function to be used by `Ec.Cart`
+
+  """
   def new( name, description, function ) do
     %Ec.Cart.Adjustment{ name: name, description: description, function: function }
   end
