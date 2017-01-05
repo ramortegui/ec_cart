b606a38998d11ef489e2700f416576b35eec8854
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-01-04 22:52:24 -0700

    Add metadata in order to publish with hex.

--- mix.exs
+++ mix.exs
@@ -7,26 +7,30 @@ defmodule Ec.Cart.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
+     description: description(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    "E-commerce cart for Elixir."
+  end
+  
+  defp package do
+    [# These are the default files included in the package
+     name: :ec_cart,
+     files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Ruben Amortegui"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/ramortegui/ec_cart" }
+    ]
   end
 end
