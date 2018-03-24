a31330a858febacf81e44d6519908c0a759042d2
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-03-23 21:21:49 -0600

    Revert "Add application description."
    This reverts commit 1aad1d8f62d07ec68c7cdfd0b30209daa996353e.

--- mix.exs
+++ mix.exs
@@ -4,12 +4,12 @@ defmodule Ec.Cart.Mixfile do
   def project do
     [
       app: :ec_cart,
-      version: "0.1.4",
+      version: "0.1.3",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       package: package(),
-      description: "OTP cart application"),
+      description: description(),
       deps: deps()
     ]
   end
