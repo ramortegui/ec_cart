436a9eb6c0c1e0764e3ecae100d0c0da0e88533d
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-03-23 23:17:00 -0600

    Update version to 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ec.Cart.Mixfile do
   def project do
     [
       app: :ec_cart,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
