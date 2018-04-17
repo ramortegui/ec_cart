765864aeaa7228fee3dd29349bb14d97f0b71a60
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-04-17 11:30:20 -0600

    Fix warning on supervisor definition.

--- lib/ec_cart/supervisor.ex
+++ lib/ec_cart/supervisor.ex
@@ -1,12 +1,12 @@
 defmodule EcCart.Supervisor do
   use Supervisor
 
-  def init(_) do
+  def init(:ok) do
     processes = [worker(EcCart.Cache, [])]
     supervise(processes, strategy: :one_for_one)
   end
 
   def start_link(_arg) do
-    Supervisor.start_link(__MODULE__, nil)
+    Supervisor.start_link(__MODULE__, :ok, [])
   end
 end
