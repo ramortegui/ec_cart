3a232e284c48c193615092a6ad5e72393dc0a3cd
mithereal <mithereal@gmail.com>
2022-11-23 00:57:47 -0700

    use registry for cache

--- lib/ex_cart/cache.ex
+++ lib/ex_cart/cache.ex
@@ -3,15 +3,19 @@ defmodule ExCart.Cache do
 
   alias ExCart.Registry
 
-  def server_process(ex_cart_server_name) do
+  def register(ex_cart_server_name) do
     Registry.register(ex_cart_server_name)
+  end
+
+  def server_process(ex_cart_server_name) do
+    via_tuple(ex_cart_server_name)
   end
 
   def remove_process(ex_cart_server_name) do
     Registry.unregister(ex_cart_server_name)
   end
 
-  defp via_tuple(name) do
+  def via_tuple(name) do
     {:via, Registry, {@registry_name, name}}
   end
 end
